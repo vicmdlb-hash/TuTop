d90e848d946c11520abb988c76ce9d08a1fc271a
vicmdlb-hash <vicmdlb@gmail.com>
2026-09-06 20:04:47 -0600

    fix: install Firebase session hardening at bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './services/firebaseSessionHardeningBridge';
 import App from './App';
 import './index.css';
 
