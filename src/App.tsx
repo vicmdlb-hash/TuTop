ed925b428a8ba9cfd6d835ed5022571e23c39c39
vicmdlb-hash <vicmdlb@gmail.com>
2026-09-07 10:15:04 -0600

    perf: enable lean V2 inbox chat summaries

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import PhysicalQaPanel from './components/PhysicalQaPanel';
 import './services/nationalBackendCanonicalBridge';
 import './services/rateLimitedOnlineBridge';
 import './services/canonicalStoreBridge';
+import './services/v2LeanChatSnapshotBridge';
 import './services/nationalIdentityHydrationBridge';
 import './services/notificationReceiptStoreBridge';
 import './services/physicalQaTelemetry';
