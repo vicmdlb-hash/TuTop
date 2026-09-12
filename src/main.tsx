f83f0e8d35462a46411e83dc6879981d04c2f78b
vicmdlb-hash <vicmdlb@gmail.com>
2026-09-12 13:31:02 -0600

    feat: initialize TuTop 0.9.1 theme before app render

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { initializeTheme091 } from './lib/theme091';
 import { getFirebaseConfig } from './services/runtimeConfig';
 import { restoreNativeSessionForProject } from './services/nativeSecureSession';
 import './index.css';
 import './brand091.css';
 
 async function bootstrap() {
+  initializeTheme091();
   const config = getFirebaseConfig();
   await restoreNativeSessionForProject(config.projectId).catch(() => ({ restored: false, migrated: false }));
   await import('./services/nativeSecureSessionBridge');
